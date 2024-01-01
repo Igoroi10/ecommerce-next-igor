d7df9e74673ec523005bc585e37f6ab4dfa24444
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2024-01-01 17:00:06 +0100

    Side photoGallery implementation at product page

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { fullProduct } from "@/app/interface";
 import { client } from "@/app/sanity";
+import ImageGallery from "@/components/ImageGallery";
 
 async function getData(slug: string) {
     const query = `*[_type == "product" && slug.current == "${slug}"][0] {
@@ -21,7 +22,13 @@ async function getData(slug: string) {
 export default async function ProductPage({params}: {params: {slug: string}}) {
     const data: fullProduct = await getData(params.slug)
     return(
-        <h1>Product page</h1>
+        <div className="bg-white">
+            <div className="mx-auto max-w-screen-xl px-4 md:px-8">
+                <div className="grid gap-8 md:grid-cols-2">
+                    <ImageGallery images={data.images}/>
+                </div>
+            </div>
+        </div>
     )
 
 }
