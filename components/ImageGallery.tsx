637dcb2e940d79d0c3c761a48b9cb28f5f1a7264
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2024-01-01 17:10:08 +0100

    Bigger image render in product page

--- components/ImageGallery.tsx
+++ components/ImageGallery.tsx
@@ -1,11 +1,15 @@
+"use client";
+
 import { urlFor } from "@/app/sanity"
 import Image from "next/image"
+import { useState } from "react"
 
 interface iAppProps {
     images: any
 }
 
 export default function ImageGallery({images}: iAppProps){
+    const [bigImage, setImage] = useState(images[0])
     return(
         <div className="grid gap-4 lg:grid-cols-5">
             <div className="order-last flex gap-4 lg:order-none lg:flex-col">
@@ -20,6 +24,15 @@ export default function ImageGallery({images}: iAppProps){
                         />
                     </div>
                 ))}
+            </div>
+            <div className="relative overflow-hidden rouded-lg bg-gray-100 lg:col-span-4">
+                        <Image 
+                            src={urlFor(bigImage).url()} 
+                            width={500} 
+                            height={500} 
+                            alt="big photo" 
+                            className="h-full w-full object-cover object-center cursor-pointer"
+                        />  
             </div>
         </div>
     )
