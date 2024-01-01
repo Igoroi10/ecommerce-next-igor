4db74c1e00b2eb52066144e952054bb5e47b9590
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2024-01-01 17:51:38 +0100

    Category query

---
+++ app/[category]/page.tsx
@@ -0,0 +1,20 @@
+import { client } from "../sanity";
+
+async function getData(category: string) {
+    const query = `*[_type == "product" && category->name == "${category}"] {
+          _id,
+            "imageUrl": images[0].asset->url,
+            price,
+            name,
+            "slug": slug.current,
+            "categoryName": category->name
+        }`;
+  
+    const data = await client.fetch(query);
+  
+    return data;
+}
+
+export default async function CategoryPage(){
+    
+}
