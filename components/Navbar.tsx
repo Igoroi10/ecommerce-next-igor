5ff8da7545580c7e5d12f1c5e8df0869a2346e51
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2024-01-01 12:31:32 +0100

    Categories at navbar with hover and selection

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,17 @@
+"use client";
+
 import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+const links = [
+    {name: 'Home', href:'/'},
+    {name: 'Men', href:'/Men'},
+    {name: 'Women', href:'/Women'},
+    {name: 'Teens', href:'/Teens'}
+]
 
 export default function Navbar() {
+    const pathName = usePathname()
     return(
         <header className="mb-8 border-b">
             <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
@@ -9,6 +20,22 @@ export default function Navbar() {
                         Test<span className="text-primary">Commerce</span>
                     </h1>
                 </Link>
+                <nav className="hidden gap-12 lg:flex 2xl:ml-16">
+                    {links.map((link, i) =>(
+                        <div key={i}>
+                            {pathName === link.href?(
+                                <Link className="text-lg font-semibold text-primary" href={link.href}>
+                                    {link.name}
+                                </Link>
+                            ):
+                            (
+                                <Link className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-primary" href={link.href}>
+                                    {link.name}
+                                </Link>
+                            )}
+                        </div>
+                    ))}
+                </nav>
             </div>
         </header>
     )
