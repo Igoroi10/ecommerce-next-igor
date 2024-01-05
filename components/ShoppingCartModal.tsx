430de9b9aa6cf6dcdecbc4c3bc1c32ccf75490af
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2024-01-05 17:01:31 +0100

    Sheet appearing at rigth side

---
+++ components/ShoppingCartModal.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui/button"
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet"
+
+
+
+export default function ShoppingCartModal(){
+    return (
+        <Sheet defaultOpen>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>Edit profile</SheetTitle>
+            </SheetHeader>
+                <div>
+                    <h1>Hi</h1>
+                </div>
+          </SheetContent>
+        </Sheet>
+      )
+}
