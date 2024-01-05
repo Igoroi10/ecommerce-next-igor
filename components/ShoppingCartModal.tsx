70ffd8697841194288db421d61eff78c45e98942
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2024-01-05 18:32:00 +0100

    Continue shopping button

--- components/ShoppingCartModal.tsx
+++ components/ShoppingCartModal.tsx
@@ -66,6 +66,12 @@ export default function ShoppingCartModal(){
                         <div className="mt-6">
                             <Button className="w-full">Checkout</Button>
                         </div>
+                        <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+                            <p>
+                                OR <button className="font-medium text-primary hover:text-primary/80" onClick={()=>handleCartClick()}>Continue Shopping</button>
+                            </p>
+
+                        </div>
                     </div>
                 </div>
           </SheetContent>
