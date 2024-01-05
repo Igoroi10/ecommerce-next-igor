8aed694fcf9025dba84e1356b88f869d1b819856
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2024-01-05 17:12:07 +0100

    Cart sheet appearing on click

--- components/ShoppingCartModal.tsx
+++ components/ShoppingCartModal.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
 import {
   Sheet,
@@ -5,18 +7,30 @@ import {
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet"
+import { useShoppingCart } from "use-shopping-cart"
 
 
 
 export default function ShoppingCartModal(){
+    const {cartCount, shouldDisplayCart, handleCartClick} = useShoppingCart()
     return (
-        <Sheet defaultOpen>
-          <SheetContent>
+        <Sheet open={shouldDisplayCart} onOpenChange={()=>handleCartClick()}>
+          <SheetContent className="sm:max-w-lg w-[90vw]">
             <SheetHeader>
-              <SheetTitle>Edit profile</SheetTitle>
+              <SheetTitle>Shopping Cart</SheetTitle>
             </SheetHeader>
-                <div>
-                    <h1>Hi</h1>
+                <div className="h-full flex flex-col justify-between">
+                    <div className="mt-8 flex-1 overflow-y-auto">
+                        <ul className="-my-6 divide-y divide-gray-200">
+                            {cartCount === 0?(
+                                <h1 className="py-6">You don't have any items</h1>
+                            ):(
+                                <h1>Items detected</h1>
+                            )}
+                        </ul>
+
+                    </div>
+
                 </div>
           </SheetContent>
         </Sheet>
